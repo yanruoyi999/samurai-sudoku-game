27300bc2a1358c6cc05ac4c58cee74717ccb20e9
yanruoyi999 <yanruoyi999@gmail.com>
2026-07-11 17:16:56 +0800

    test: fix printable export assertion typing

--- app/[locale]/games/samurai/printable/[id]/page.test.ts
+++ app/[locale]/games/samurai/printable/[id]/page.test.ts
@@ -4,7 +4,7 @@ import * as printablePuzzlePage from "./page";
 
 describe("printable puzzle page prerendering", () => {
   it("does not prebuild every noindex dated printable page", () => {
-    expect(printablePuzzlePage.generateStaticParams).toBeUndefined();
+    expect(Reflect.has(printablePuzzlePage, "generateStaticParams")).toBe(false);
     expect(printablePuzzlePage.dynamicParams).toBe(true);
     expect(printablePuzzlePage.revalidate).toBe(86_400);
   });
